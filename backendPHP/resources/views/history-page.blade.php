d1bc524209f6fe59a50631470f0fc8c14ecb4d69
IRISnoir <hoangphantom2468@gmail.com>
2025-08-17 18:38:45 +0700

    đổi định dạng mỗi khung lịch sử

--- backendPHP/resources/views/history-page.blade.php
+++ backendPHP/resources/views/history-page.blade.php
@@ -21,9 +21,11 @@
     <body>
         @forelse($history as $item)
         <a href="{{ route('history-content', $item->summaryid) }}">
-            <div style="border: 1px solid #696c71; border-radius: 5px; margin-bottom: 10px; padding: 5px;">
-            <p>{{ $item->title }}</p>
-            <p>{{ $item->summary_ratio }}</p>
+        <div style="border: 1px solid #696c71; border-radius: 5px; margin-bottom: 10px; padding: 5px;">
+            <h4 style="font-weight: bold; font-style: italic; text-decoration: underline; text-align: center;">{{ $item->file_name }}</h4>
+            <p style="text-align: justify;">{{ $item->title }}</p>
+            <p style="text-align: right; font-size: 0.8rem; color: #696c71;">Tỉ lệ: {{ $item->summary_ratio }}</p>
+            <p style="text-align: right; font-size: 0.8rem; color: #696c71;">Ngày tạo: {{ $item->created_at }}</p>
         </div>
         </a>
         @empty
