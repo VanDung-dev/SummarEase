3c2cf582a537946ff065678376c991c5dc7211a5
DangKhoa <150508728+khoacoder3110@users.noreply.github.com>
2025-08-22 13:51:48 +0700

    xóa file style.css và chỉnh sidebar

--- backendPHP/resources/views/welcome.blade.php
+++ backendPHP/resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link href="style.css" rel="stylesheet" />
+
 
         @include('partials.head')
 
